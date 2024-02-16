b1823a77c96f38f2119660daef43fd6da57bcc45
Gustx21 <145871288+Gustx21@users.noreply.github.com>
2024-02-15 21:10:16 -0300

    PERF

--- aula05/funções-fluxo.js
+++ aula05/funções-fluxo.js
@@ -2,38 +2,34 @@ export function subtrairTotal() {
     const receitaTotal = parseInt(prompt("Digite o número total de receitas:"));
     let resultadoValor1 = [];
     let soma = 0;
-    let i = 0;
     let calculo;
 
-    for (; a < receitaTotal; i++) {
-        let receitas = parseInt(prompt(`Digite os valores de cada receita por ordem (${i}):`));
+    for (let i = 0; i < receitaTotal; i++) {
+        let receitas = parseInt(prompt(`Digite os valores de cada receita por ordem (${i + 1}):`));
 
         resultadoValor1.push(receitas);
     }
 
-    while (i < resultadoValor1.length) {
-        soma += resultadoValor1[i];
-        i++
-    }
+    resultadoValor1.forEach(function(item1, indece1) {
+        return soma += resultadoValor1[indece1];
+    });
 
     const gastoTotal = parseInt(prompt("Digite o número total de gastos:"));
     let resultadoValor2 = [];
     let subtrair = 0;
 
-    while (i < gastoTotal) {
-        let gastos = parseInt(prompt(`Digite os valores de cada gasto por ordem (${i}):`));
+    for (let i = 0; i < gastoTotal;) {
+        let gastos = parseInt(prompt(`Digite os valores de cada gasto por ordem (${i + 1}):`));
 
         resultadoValor2.push(gastos);
         i++
     }
-    
-    for (; o < resultadoValor2.length; i++) {
-        subtrair += resultadoValor2[i];
-    }
+
+    resultadoValor2.forEach(function(item2, indece2) {
+        return subtrair += resultadoValor2[indece2];
+    });
 
     calculo = soma - subtrair;
 
-    alert(`O valor total das receitas é ${soma}`);
-    alert(`O resultado dos gastos é ${subtrair}`);
-    alert(`O resultado no dinherio livre é ${calculo}`)
+    alert(`O resultado do dinheiro livre é ${calculo}`);
 };
