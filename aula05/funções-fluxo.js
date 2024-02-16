cbefbf2569966ca7c9e46c141fc71ef814e2e5bc
Gustx21 <145871288+Gustx21@users.noreply.github.com>
2024-02-16 16:53:46 -0300

    Update funções-fluxo.js

--- aula05/funções-fluxo.js
+++ aula05/funções-fluxo.js
@@ -1,11 +1,14 @@
+const resultadoValor1 = [];
+const resultadoValor2 = [];
+let soma = 0;
+let subtrair = 0;
+let calculo, receitas;
+
 export function subtrairTotal() {
     const receitaTotal = parseInt(prompt("Digite o número total de receitas:"));
-    let resultadoValor1 = [];
-    let soma = 0;
-    let calculo;
 
     for (let i = 0; i < receitaTotal; i++) {
-        let receitas = parseInt(prompt(`Digite os valores de cada receita por ordem (${i + 1}):`));
+        receitas = parseInt(prompt(`Digite os valores de cada receita por ordem (${i + 1}):`));
 
         resultadoValor1.push(receitas);
     }
@@ -15,8 +18,6 @@ export function subtrairTotal() {
     });
 
     const gastoTotal = parseInt(prompt("Digite o número total de gastos:"));
-    let resultadoValor2 = [];
-    let subtrair = 0;
 
     for (let i = 0; i < gastoTotal;) {
         let gastos = parseInt(prompt(`Digite os valores de cada gasto por ordem (${i + 1}):`));
