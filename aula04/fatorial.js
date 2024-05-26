4ef932c2cbc211a0dcadc993641ab0040c36856e
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-05-26 16:22:46 -0300

    Corrigindo a função

--- aula04/fatorial.js
+++ aula04/fatorial.js
@@ -1,22 +1,26 @@
 // Function chamada com evento de click
 function calcular() {
     // Variáveis armazenando valores inseridos
-    let num = document.getElementById('numero');
-    const res = document.getElementById('resposta');
-
+    const num = document.getElementById('numero');
+    
     // Declarando variáveis com valores de entrada
     let fatorial = 1;
-    let i = 1;
     
     // Convertendo para tipo numérico
     num = Number(num.value);
-
+    
     // Loop para descobrir o fatorial
-    for (; i <= num;) {
-        fatorial *= i;
+    for (let i = 1; i <= num;) {
+        fatorial = fatorial * i;
         i++;
     };
 
+    exibir();
+}
+
+function exibir() {
+    const res = document.getElementById('resposta');
+
     // Acrescentando os valores no conteúdo
-    res.innerHTML = `O fatorial de ${num} é ${fatorial}.`
+    res.innerHTML = `O fatorial de ${num} é ${fatorial}.`;
 }
